d75b0f882d185d0f3f9f2a3bb05a26362783980a
Christian Armstrong <christianarmstrong@christians-mbp.devices.brown.edu>
2023-04-23 01:14:43 -0400

    I can not run this on my device

---
+++ alpaca.py
@@ -0,0 +1,43 @@
+import torch
+from langchain import LLMChain, PromptTemplate
+from langchain.llms import HuggingFacePipeline
+from transformers import (GenerationConfig, LlamaForCausalLM, LlamaTokenizer,
+                          pipeline)
+
+tokenizer = LlamaTokenizer.from_pretrained("chavinlo/alpaca-native")
+
+base_model = LlamaForCausalLM.from_pretrained(
+    "chavinlo/alpaca-native",
+    load_in_8bit=True,
+    device_map='auto',
+)
+
+pipe = pipeline(
+    "text-generation",
+    model=base_model,
+    tokenizer=tokenizer,
+    max_length=256,
+    temperature=0.6,
+    top_p=0.95,
+    repetition_penalty=1.2
+)
+
+local_llm = HuggingFacePipeline(pipeline=pipe)
+
+
+template = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+### Instruction: 
+{instruction}
+
+Answer:"""
+
+prompt = PromptTemplate(template=template, input_variables=["instruction"])
+
+llm_chain = LLMChain(prompt=prompt,
+                     llm=local_llm
+                     )
+
+question = "What is the capital of England?"
+
+print(llm_chain.run(question))
